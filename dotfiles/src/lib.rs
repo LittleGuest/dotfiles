4cf5bff396ea544088933e6d89c8810133831272
LittleGuest <2190975784@qq.com>
2024-11-03 14:41:38 +0000

    update dotfiles-setup

--- dotfiles/src/lib.rs
+++ dotfiles/src/lib.rs
@@ -1,7 +1,6 @@
 use cmd::Cmd;
 use serde::Deserialize;
 use symlink::Symlink;
-use tokio::task::JoinSet;
 
 mod cmd;
 mod symlink;
@@ -30,19 +29,15 @@ impl App {
 
         let mut failed_cmds = Vec::new();
         if let Some(cmds) = self.cmds {
-            let mut cmd_set = JoinSet::new();
             for cmd in cmds.into_iter().map(Cmd::new) {
-                cmd_set.spawn(cmd);
+                failed_cmds.push(cmd.await);
             }
-            failed_cmds.append(&mut cmd_set.join_all().await);
         }
 
         if let Some(symlinks) = self.symlinks {
-            let mut symlink_set = JoinSet::new();
             for symlink in symlinks {
-                symlink_set.spawn(symlink);
+                symlink.await;
             }
-            symlink_set.join_all().await;
         }
 
         if failed_cmds.is_empty() {
