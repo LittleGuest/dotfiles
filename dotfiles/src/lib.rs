74dd04dde9292becb0baa8f3531b622822548d8e
LittleGuest <2190975784@qq.com>
2024-11-03 11:33:36 +0000

    update dotfiles-setup

--- dotfiles/src/lib.rs
+++ dotfiles/src/lib.rs
@@ -8,9 +8,9 @@ mod symlink;
 
 #[derive(Debug, Deserialize)]
 pub struct App {
-    pre: Vec<String>,
-    cmds: Vec<String>,
-    symlinks: Vec<Symlink>,
+    pre: Option<Vec<String>>,
+    cmds: Option<Vec<String>>,
+    symlinks: Option<Vec<Symlink>>,
 }
 
 impl App {
@@ -19,35 +19,46 @@ impl App {
     }
 
     pub async fn run(self) {
-        for cmd in self.pre.into_iter().map(Cmd::new) {
-            if let Some((c, err)) = cmd.await {
-                eprintln!("{c}，原因：{err}");
-                return;
+        if let Some(pre) = self.pre {
+            for cmd in pre.into_iter().map(Cmd::new) {
+                if let Some((c, err)) = cmd.await {
+                    eprintln!("{c}，原因：{err}");
+                    return;
+                }
             }
         }
 
-        // let mut cmd_set = JoinSet::new();
-        // for cmd in self.cmds.into_iter().map(Cmd::new) {
-        //     cmd_set.spawn(cmd);
-        // }
-        // let res = cmd_set.join_all().await;
-        //
-        // let mut symlink_set = JoinSet::new();
-        // for symlink in self.symlinks {
-        //     symlink_set.spawn(symlink);
-        // }
-        // symlink_set.join_all().await;
-        //
-        // let fail_cmds = res
-        //     .iter()
-        //     .filter(|c| c.is_some())
-        //     .flatten()
-        //     .collect::<Vec<_>>();
-        // if !fail_cmds.is_empty() {
-        //     eprintln!("\n\n\n以下命令执行失败");
-        //     for (cmd, e) in fail_cmds {
-        //         eprintln!("{cmd}，原因：{e}");
-        //     }
-        // }
+        let mut failed_cmds = Vec::new();
+        if let Some(cmds) = self.cmds {
+            let mut cmd_set = JoinSet::new();
+            for cmd in cmds.into_iter().map(Cmd::new) {
+                cmd_set.spawn(cmd);
+            }
+            failed_cmds.append(&mut cmd_set.join_all().await);
+        }
+
+        if let Some(symlinks) = self.symlinks {
+            let mut symlink_set = JoinSet::new();
+            for symlink in symlinks {
+                symlink_set.spawn(symlink);
+            }
+            symlink_set.join_all().await;
+        }
+
+        if failed_cmds.is_empty() {
+            return;
+        }
+
+        let fail_cmds = failed_cmds
+            .iter()
+            .filter(|c| c.is_some())
+            .flatten()
+            .collect::<Vec<_>>();
+        if !fail_cmds.is_empty() {
+            eprintln!("\n\n\n以下命令执行失败");
+            for (cmd, e) in fail_cmds {
+                eprintln!("{cmd}，原因：{e}");
+            }
+        }
     }
 }
